addc70dbe5925c12b39e64ff2e55b87357d29422
Shadaj Laddad <shadaj@users.noreply.github.com>
2021-01-29 11:52:18 -0800

    Switch to GitHub Actions for CI (#5)

--- publish.sh
+++ publish.sh
@@ -2,8 +2,14 @@
 
 set -e # exit with nonzero exit code if anything fails
 
-openssl aes-256-cbc -K $encrypted_a12df543abe9_key -iv $encrypted_a12df543abe9_iv -in secrets.tar.enc -out secrets.tar -d
+openssl aes-256-cbc -K $encrypted_key -iv $encrypted_iv -in secrets.tar.enc -out secrets.tar -d
 
 tar xvf secrets.tar
 
-sbt publishSigned sonatypeRelease
+echo $PGP_PASSPHRASE | gpg --passphrase-fd 0 --batch --yes --import publishing-setup/private.key
+
+export GPG_TTY=/dev/ttys003
+
+cp publishing-setup/credentials.sbt credentials.sbt
+
+sbt publishSigned sonatypeBundleRelease
